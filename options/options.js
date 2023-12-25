b8ac51001a5b7ac2c17c6b8bf443a6a23f0e7b4a
Crowdbotics <team@crowdbotics.com>
2023-12-25 18:55:25 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "music-and-arts-temp-45189",
+  url: "https://music-and-arts-temp-45189.botics.co",
+  api: "https://music-and-arts-temp-45189.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
